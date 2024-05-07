adb54fc4d88d5b2dafbb32c50d812b038e647821
SeworL <rnjsalsrb6666@naver.com>
2024-05-07 19:57:21 +0900

    0507

--- CppProgrammingandPractice/9week/MapWithPersonClass/Person.h
+++ CppProgrammingandPractice/9week/MapWithPersonClass/Person.h
@@ -1,32 +1,48 @@
 #include <string>
 
-class Person {
- public:
-  Person(std::string name, size_t age) : name{name}, age{age} {
+class Person
+{
+public:
+  Person(std::string name, size_t age) : name{name}, age{age}
+  {
     // set ID with uniqueness
+    static long long id = 0;
+    ID = ++id;
   }
 
-  friend std::ostream& operator<<(std::ostream& out, const Person& p) {
+  friend std::ostream &operator<<(std::ostream &out, const Person &p)
+  {
     // implementation
+    out << p.ID << " " << p.name << " " << p.age;
+    return out;
   }
 
-  bool operator<(const Person& rhs) const {
+  bool operator<(const Person &rhs) const
+  {
     // implementation
+    return ID < rhs.ID;
   }
 
-  class ComparatorByAge {
-   public:
-    bool operator()(const Person& lhs, const Person& rhs) const {
+  class ComparatorByAge
+  {
+  public:
+    bool operator()(const Person &lhs, const Person &rhs) const
+    {
       // implementation
+      return lhs.age < rhs.age;
     }
   };
 
-  class ComparatorByName {
-   public:
+  class ComparatorByName
+  {
+  public:
     // implementation
+    bool operator()(const Person &lhs, const Person &rhs) const {
+      return lhs.name.compare(rhs.name) < 0;
+    }
   };
 
- private:
+private:
   long long ID;
   std::string name;
   size_t age;
