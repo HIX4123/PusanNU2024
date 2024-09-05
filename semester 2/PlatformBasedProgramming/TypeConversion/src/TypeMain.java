1ba5a30c2ff75852796f84236f6c722c48876401
SeworL <rnjsalsrb6666@naver.com>
2024-09-05 11:20:30 +0900

    feat: adding Type Conversion project

---
+++ semester 2/PlatformBasedProgramming/TypeConversion/src/TypeMain.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class TypeMain {
+
+
+    public static void main(String[] args) {
+        /* Your code here!*/
+        // 스캐너로 입력 받기
+        // printType() 호출
+        // 형변환 ( String --> int)
+        // 형변환 ( String --> float)
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        printType(input);
+        System.out.println("Type conversion to int...");
+        printType(Integer.parseInt(input));
+        System.out.println("Type conversion to float...");
+        printType(Float.parseFloat(input));
+    }
+
+    private static void printType(String x) {
+        System.out.println(x + " is a string");
+    }
+
+    private static void printType(int x) {
+        System.out.println(x + " is an int");
+    }
+
+    private static void printType(float x) {
+        System.out.println(x + " is a float");
+    }
+
+
+}
