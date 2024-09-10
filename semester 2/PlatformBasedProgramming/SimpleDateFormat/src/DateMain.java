50975bd4a8af431a061657d8ee9b145a82dbbcec
SeworL <rnjsalsrb6666@naver.com>
2024-09-10 11:36:59 +0900

    feat: adding DateMain Class

--- semester 2/PlatformBasedProgramming/SimpleDateFormat/src/DateMain.java
+++ semester 2/PlatformBasedProgramming/SimpleDateFormat/src/DateMain.java
@@ -8,8 +8,8 @@ public class DateMain {
     public static void main(String[] args) {
         Date today = new Date();
 
-        System.out.println(new SimpleDateFormat("yyyy년 mm월 dd일").format(today));
-        System.out.println(new SimpleDateFormat("yyyy-mm-dd HH:mm:ss").format(today));
+        System.out.println(new SimpleDateFormat("yyyy년 MM월 dd일").format(today));
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(today));
         System.out.println(new SimpleDateFormat("오늘은 이 달의 F번째 E요일 입니다.", Locale.KOREAN).format(today));
     }
 
